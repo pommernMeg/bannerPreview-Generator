6a221e00f51f8cc84d3582d387d714e469a3d9ab
Marcel Eggert <m.eggert@heinlein-support.de>
2022-10-22 14:09:21 +0200

    [Change] resize preview option

--- generator.py
+++ generator.py
@@ -2,12 +2,16 @@
 
 """generator.py: Python script to create a banner preview from the images for the missions of Ingress."""
 
+import getopt
+import glob
+import os
+import subprocess
+import sys
+
 import PIL
 from PIL import Image
-import glob
 
-import os, sys
-
+Image.MAX_IMAGE_PIXELS = None
 
 final_size = 512
 
@@ -37,86 +41,121 @@ def resize_aspect_fit(folder):
              new_im.paste(im, (0,0))
              new_im.save(f + 'resized.png', 'PNG', quality=100)
 
-for folder in next(os.walk('input'))[1]:
-	print(folder)
-	outputFolder = f'output/{folder}'
+def resize_preview( outputFolder, folder, scale):
+	file = f"{outputFolder}/{folder}_preview.png"
 
-	folderExist = os.path.exists(outputFolder)
-	if not folderExist:
-	
-		resize_aspect_fit(folder)
+	if os.path.isfile(file):
+		im = Image.open(file)
+		size = im.size
+		im = im.resize([int(size[0] * (scale/100)), int(size[1] * (scale/100)) ], resample=5)
+		new_im = Image.new("RGBA", [int(size[0] * (scale/100)), int(size[1] * (scale/100))])
+		new_im.paste(im, (0,0))
+		new_im.save(f'input/{folder}/{folder}_preview.png', 'PNG', quality=100)
 
-		list_im = sorted(glob.glob(f"input/{folder}/resize/*resized.png"),reverse=True)
-		imgs    = [ PIL.Image.open(i) for i in list_im ]
+def main():
+	args = sys.argv[1:]
+	scale = 100
+	try:
+		opts, args = getopt.getopt(args,"h:s",["scale="])
+	except getopt.GetoptError:
+		print('generator.py -i <inputfile> -o <outputfile>')
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print('generator.py -s <scale>')
+			sys.exit()
+		elif opt in ("-s", "--scale"):
+			scale = int(args[0])
 
-		print(f"Pictures in Folder: {len(list_im)}")
 
-		lines = len(list_im) // 6
-		print(f"Lines: {lines}")
+	for folder in next(os.walk('input'))[1]:
+		print(folder)
+		outputFolder = f'output/{folder}'
 
-
-		# Check whether the specified path exists or not
-		isExist = os.path.exists(outputFolder)
-
-		if not isExist:
-			# Create a new directory because it does not exist 
-			os.makedirs(outputFolder)
-
-		y = 544
-		x = 3232
+		folderExist = os.path.exists(outputFolder)
+		if not folderExist:
 		
-		new_im = Image.new("RGBA", [x, y * lines])
-		temp = Image.open("template/6er maske.png")
+			resize_aspect_fit(folder)
 
-		x = 0
-		y = 0
+			list_im = sorted(glob.glob(f"input/{folder}/resize/*resized.png"),reverse=True)
+			imgs    = [ PIL.Image.open(i) for i in list_im ]
 
-		for i in range(1,lines +1):
+			print(f"Pictures in Folder: {len(list_im)}")
 
-			new_im.paste(temp, (x, y))
-			y += 544
-		
-		new_im.save( f'{outputFolder}/{folder}_preview.png' )
+			lines = len(list_im) // 6
+			print(f"Lines: {lines}")
 
-		temp = Image.open("template/6er maske.png")
 
-		pos_x = 0
-		pos_y = 16
-		
-		cnt = 1
+			# Check whether the specified path exists or not
+			isExist = os.path.exists(outputFolder)
 
-		for img in imgs:
+			if not isExist:
+				# Create a new directory because it does not exist 
+				os.makedirs(outputFolder)
 
-			if (cnt % 6) == 0:
-				# print("Add Picture")
-				new_im.paste(img, (pos_x, pos_y))
-				pos_x += img.size[0]
-				# print("New Line")
-				pos_x = 0
-				pos_y += img.size[1]
-				pos_y += 32
+			y = 544
+			x = 3232
 			
-			else:
-				# print("Add Picture")
-				new_im.paste(img, (pos_x, pos_y))
-				pos_x += img.size[0]
-				pos_x += 32
+			new_im = Image.new("RGBA", [x, y * lines])
+			temp = Image.open("template/6er maske.png")
 
-			cnt += 1
+			x = 0
+			y = 0
 
-		new_im.save( f'{outputFolder}/{folder}_background.png' )
+			for i in range(1,lines +1):
 
-		# Opening the primary image (used in background)
-		img1 = Image.open(f"{outputFolder}/{folder}_background.png")
-		
-		# Opening the secondary image (overlay image)
-		img2 = Image.open( f'{outputFolder}/{folder}_preview.png' )
-		
-		# Pasting img2 image on top of img1 
-		# starting at coordinates (0, 0)
-		img1.paste(img2, (0,0), mask = img2)
+				new_im.paste(temp, (x, y))
+				y += 544
+			
+			new_im.save( f'{outputFolder}/{folder}_preview.png' )
 
-		img1.save( f'{outputFolder}/{folder}_preview.png' )
+			temp = Image.open("template/6er maske.png")
 
-		os.remove(f"{outputFolder}/{folder}_background.png")
+			pos_x = 0
+			pos_y = 16
+			
+			cnt = 1
 
+			for img in imgs:
+
+				if (cnt % 6) == 0:
+					# print("Add Picture")
+					new_im.paste(img, (pos_x, pos_y))
+					pos_x += img.size[0]
+					# print("New Line")
+					pos_x = 0
+					pos_y += img.size[1]
+					pos_y += 32
+				
+				else:
+					# print("Add Picture")
+					new_im.paste(img, (pos_x, pos_y))
+					pos_x += img.size[0]
+					pos_x += 32
+
+				cnt += 1
+
+			new_im.save( f'{outputFolder}/{folder}_background.png' )
+
+			# Opening the primary image (used in background)
+			img1 = Image.open(f"{outputFolder}/{folder}_background.png")
+			
+			# Opening the secondary image (overlay image)
+			img2 = Image.open( f'{outputFolder}/{folder}_preview.png' )
+			
+			# Pasting img2 image on top of img1 
+			# starting at coordinates (0, 0)
+			img1.paste(img2, (0,0), mask = img2)
+
+			img1.save( f'{outputFolder}/{folder}_preview.png' )
+
+			os.remove(f"{outputFolder}/{folder}_background.png")
+
+
+			subprocess.run(f"rm -rf input/{folder}/resize", shell=True)
+
+			if( scale != 100):
+				resize_preview( outputFolder, folder , scale )
+
+if __name__ == "__main__":
+	main()
